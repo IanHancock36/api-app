0712c1888f259ca183a4307cdc706a18d009ad17
Brendan <roman.bp15@gmail.com>
2021-05-24 20:44:22 -0400

    brendan fixer upper but not really tho

--- src/App.js
+++ src/App.js
@@ -1,51 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Crypto from './Crypto';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Crypto from "./Crypto";
+import "./App.modules.css";
+
 function App() {
-	const [ cryptos, setCryptos ] = useState([]);
-	const [ search, setSearch ] = useState('');
-	useEffect(() => {
-		axios
-			.get(
-				'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-			)
-			.then((res) => {
-				setCryptos(res.data);
-			})
-			.catch((error) => console.log(error));
-	}, []);
+  const [cryptos, setCryptos] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then(res => {
+        setCryptos(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
-	const handleChange = (event) => {
-		setSearch(event.target.value);
-		console.log(event.target.value);
-	};
-	const filteredCryptos = cryptos.filter((crypto) => crypto.name.toLowerCase().includes(search.toLowerCase()));
-	return (
-		<div>
-			<div className="crypto-search">
-				<h1 className="crypto-text"> Seawolf Crypto</h1>
-				<form>
-					<input type="text" placeholder="Search" className="crypto-input" onChange={handleChange} />
-				</form>
-			</div>
+  const handleChange = event => {
+    setSearch(event.target.value);
+    console.log(event.target.value);
+  };
+  const filteredCryptos = cryptos.filter(crypto =>
+    crypto.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div className="app">
+      <div className="crypto-search">
+        <h1 className="crypto-text"> Seawolf Crypto</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="crypto-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
 
-			{filteredCryptos.map((crypto) => {
-				return (
-					<Crypto
-						key={crypto.id}
-						name={crypto.name}
-						price={crypto.current_price}
-						symbol={crypto.symbol}
-						marketcap={crypto.total_volume}
-						image={crypto.image}
-						priceChange={crypto.price_change_percentage_24h}
-						volume={crypto.market_cap}
-					/>
-				);
-			})}
-		</div>
-	);
+      {filteredCryptos.map(crypto => {
+        return (
+          <Crypto
+            key={crypto.id}
+            name={crypto.name}
+            price={crypto.current_price}
+            symbol={crypto.symbol}
+            marketcap={crypto.total_volume}
+            image={crypto.image}
+            priceChange={crypto.price_change_percentage_24h}
+            volume={crypto.market_cap}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/Crypto.js
+++ src/Crypto.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import './Crypto.css';
+import React from "react";
+import "./cryptoItem.modules.css";
 // this is destructuring the data below. destructuring object passed as an argument in the function.
-const Crypto = ({ name, price, symbol, marketcap, volume, image, priceChange }) => {
-	return (
-		<div className="crypto-item">
-			<div className="item-cell cell-header">
-				<div>
-					<img className="cell-image" src={image} />
-				</div>
-				<div>{name}</div>
-			</div>
-			<div className="item-cell">{symbol}</div>
-			<div className="item-cell">{price}</div>
-		</div>
-	);
+const Crypto = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
+  return (
+    <div className="crypto-item">
+      <div className="item-cell">
+        <img className="cell-image" src={image} />
+      </div>
+      <div className="item-cell">
+        <div>{name}</div>
+      </div>
+      <div className="item-cell">{symbol}</div>
+      <div className="item-cell">{price}</div>
+      <div className="item-cell">
+        <p className={priceChange < 0 ? "percent-red" : "percent-green"}>
+          {priceChange.toLocaleString(2)}%
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Crypto;
@@ -32,11 +45,7 @@ export default Crypto;
 // 	<p className="crypto-volume">${volume.toLocaleString(undefined)}</p>
 // 	</div>
 // 	<div>
-// 	{priceChange < 0 ? (
-// 		<p className="crypto-percent red">{priceChange.toFixed(2)}%</p>
-// 	) : (
-// 		<p className="crypto-percent green">{priceChange.toFixed(2)}%</p>
-// 	)}
+//
 // 	<p className="crypto-marketcap">Mkt Cap : ${marketcap.toLocaleString()}</p>
 // 	</div>
 // </div>
