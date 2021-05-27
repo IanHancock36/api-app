3410aac6694f7440446f94b8331cd2ef0beb9aaa
Ian Hancock <northroadian@gmail.com>
2021-05-27 06:45:34 -0500

    adding stripe payment form

--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 
-const StripeCheckout = () => {
+const PaymentForm = () => {
+    const stripe = useStripe(); 
+    const elements = useElements; 
     return (
-        <div>
-            
-        </div>
+        <form onSubmit = {handleSubmit}> 
+        <CardElement />
+        <button type ='submit' disabled ={!stripe}>
+            Pay Now
+        </button>
+
+        </form>
     )
 }
 
-export default StripeCheckout
+export default PaymentForm
