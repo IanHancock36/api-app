0aae854ddb6c374c9905f32ed93e81ebda6f7594
Ian Hancock <northroadian@gmail.com>
2021-05-28 06:13:58 -0500

    adding more to payment form

--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -1,53 +1,61 @@
-import React,{useState} from 'react'
-import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
-import axios from 'axios'
+import React, { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import axios from 'axios';
 
 const PaymentForm = () => {
-    const [success , setSuccess] = uesState(false)
-    const stripe = useStripe()
-    const elements = useElements()
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        const [error, payementMethod] = await stripe.createPaymentMethod({
-            type:'card',
-            card: elements.getElement(CardElement)
-    });
+	const [ success, setSuccess ] = uesState(false);
+	const stripe = useStripe();
+	const elements = useElements();
 
-    
-    if(!error){
-        try {
-            const {id} = payementMethod
-            const response = await axios.post('http//localhost:4000/payment',{
-                amount : 1000,
-                id
-            })
-        
-              if(response.data.success){
-                  console.log('Succesful Paymemnt')
-                  setSuccess(true)
-              }
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const [ error, payementMethod ] = await stripe.createPaymentMethod({
+			type: 'card',
+			card: elements.getElement(CardElement)
+		});
 
-            
-        } catch (error) {
-            console.log("Error", error)
-            
-        }
-    } else{
-        console.log("error.message")
-    }
-    
-    return (
-        <form onSubmit ={handleSubmit} >
+		if (!error) {
+			try {
+				const { id } = payementMethod;
+				const response = await axios.post('http//localhost:4000/payment', {
+					amount: 1000,
+					id
+				});
 
-          
-            
+				if (response.data.success) {
+					console.log('Succesful Paymemnt');
+					setSuccess(true);
+				}
+			} catch (error) {
+				console.log('Error', error);
+			}
+		} else {
+			console.log('error.message');
+		}
+	};
 
-
+	return (
+        <>
+        {!success ?
+        <form onSubmit={handleSubmit}>
+           <fieldset className= 'FormGroup'>
+               <div className='FormRow'>
+                   <CardElement options={CARD_OPTIONS} />
+               </div>
+           </fieldset>
+           <button>Pay</button>
         </form>
-        
-      
-    )
-}
 
-export default PaymentForm
+        : 
+        <div>
+            <h2> You bought a great shirt.</h2>
+        </div>
+
+        
+    } 
+        </>
+    )
+
+};
+
+export default PaymentForm;
