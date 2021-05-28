6fd4fd99cccd264b94c925b9be2a88d201a8f566
Ian Hancock <northroadian@gmail.com>
2021-05-28 05:58:50 -0500

    adding try catch statement with error handling

--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -14,13 +14,27 @@ const PaymentForm = () => {
             card: elements.getElement(CardElement)
     });
 
-    }
+    
     if(!error){
         try {
-            const 
+            const {id} = payementMethod
+            const response = await axios.post('http//localhost:4000/payment',{
+                amount : 1000,
+                id
+            })
+        
+              if(response.data.success){
+                  console.log('Succesful Paymemnt')
+                  setSuccess(true)
+              }
+
+            
         } catch (error) {
+            console.log("Error", error)
             
         }
+    } else{
+        console.log("error.message")
     }
     
     return (
