be7a6c007a3706ed8b4c80a88d084e0307b90032
Ian Hancock <northroadian@gmail.com>
2021-05-28 05:41:58 -0500

    adding more to the stripe checkout

--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import axios from 'axios'
 
 const PaymentForm = () => {
-    const stripe = useStripe(); 
-    const elements = useElements; 
+    const [success , setSuccess] = uesState(false)
+    const stripe = useStripe()
+    const elements = useElements()
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const [error, payementMethod] = await stripe.createPaymentMethod({
+            type:'card',
+            card: elements.getElement(CardElement)
+    });
+
+    }
+    if(!error){
+        try {
+            const 
+        } catch (error) {
+            
+        }
+    }
+    
     return (
-        <form onSubmit = {handleSubmit}> 
-        <CardElement />
-        <button type ='submit' disabled ={!stripe}>
-            Pay Now
-        </button>
+        <form onSubmit ={handleSubmit} >
+
+          
+            
+
 
         </form>
+        
+      
     )
 }
 
