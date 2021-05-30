563dc71c8823576a6c4c596d8406c885f9eae1a3
Ian Hancock <northroadian@gmail.com>
2021-05-30 07:25:38 -0500

    more progess on table

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,6 +1,6 @@
-import { Table } from 'reactstrap';
+import { Container, Row, Col, Table } from 'reactstrap';
 import React from 'react';
-// import './cryptoItem.modules.css';
+import './cryptoItem.modules.css';
 
 // this is destructuring the data below. destructuring object passed as an argument in the function.
 const Crypto = ({ name, price, symbol, marketcap, volume, image, priceChange }) => {
@@ -17,50 +17,69 @@ const Crypto = ({ name, price, symbol, marketcap, volume, image, priceChange }) 
 		// 			<div className="item-cell">{price}</div>
 		// 			<div className="item-cell">
 		// 				<p className={priceChange < 0 ? 'percent-red' : 'percent-green'}>{priceChange.toLocaleString(2)}%</p>
-		// 				{/* <div>
+		// 				<div>
 		// 					<p className="crypto-volume">${volume.toLocaleString(undefined)}</p>
 		// 				</div>
 		// 				<div>
 		// 					<p className="crypto-marketcap">Mkt Cap : ${marketcap.toLocaleString()}</p>
-		// 				</div> */}
+		// 				</div>
 		// 			</div>
 		// 		</div>
 
 		// 	);
 		// };
 		// Below is using React strap to make a table
-		<Table responsive>
+		// 	<Container>
+		// 	<Row >
+		//     <Col><img src={image}/></Col>
+		//     <Col>Name: {name}</Col>
+		// 	<Col>Price: ${price}</Col>
+		// 	<Col>Symbol: {symbol}</Col>
+		// 	<Col>Price Change: {priceChange < 0 ? 'percent-red' : 'percent-green'}{priceChange.toLocaleString(2)}%</Col>
+		// 	<Col>Volume: ${volume.toLocaleString(undefined)}</Col>
+		// 	<Col>Mkt Cap : ${marketcap.toLocaleString()}</Col>
+		// 	</Row>
+
+		//   </Container>
+		//
 	
-			<th>
-			
-				<td>
-				<img src={image} />
-				</td>
-			<th>
-			
-				<td>{name}</td>
-			</th>
-			<th>
-			
-			<td>{symbol}</td>
-			</th>
-			
-				<th>
-			
-				<td>{price}</td>
-				</th>
+		<Table bordered responsive>
+			<thead>
+				<tr>
+					<th scope ='row'></th>
+					<th scope ='row'>Name</th>
+					<th scope ='row'>Price</th>
+					<th scope ='row'>Change</th>
+					<th scope ='row'>Volume</th>
+					<th scope ='row'>Mkt Cap</th>
 				
-				
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">
+						<img className="cell-image" src={image} />
+					</th>
+					
+				<td >{name} </td>
+				<td > ${price} </td>	
+				<td >
 			
-				{/* <td>
-				<p className={priceChange < 0 ? 'percent-red' : 'percent-green'}>{priceChange.toLocaleString(2)}%</p>
-				</td>
-				<td>${volume.toLocaleString(undefined)}</td>
-				<td>Mkt Cap : ${marketcap.toLocaleString()}</td> */}
-			</th>
-	
+						<p className={priceChange < 0 ? 'percent-red' : 'percent-green'}>
+							{priceChange.toLocaleString(2)}%
+						</p>
+					</td>
+					{/* <td>Volume:</td> */}
+					<td > ${volume.toLocaleString(undefined)}</td>
+					{/* <td>Mkt Cap:</td> */}
+					<td >${marketcap.toLocaleString()}</td>
+				</tr>
+			</tbody>
 		</Table>
+		
 	);
 };
 
 export default Crypto;
+
+/// the best solution prob is to make a grid then shrink to columns. table is not as responsive
