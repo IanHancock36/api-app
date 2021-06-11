77fac8e925d44cd01fa1aad591c7ce2dbfcfc88d
Ian Hancock <northroadian@gmail.com>
2021-06-11 15:52:32 -0500

    navbars

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,37 +1,58 @@
-
-
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Navbar.css';
+//
+	// I need a mobile close button set to false 
+		// I need a handle click set to not false !click
+			// this will use the use state open setOpen to false. 
 
 const Navbar = () => {
-    const [open,setOpen] =useState('false')
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-link'>Home</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link to='/videos' className='nav-link'>Videos</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link to='/about' className='nav-link'>About</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link to='/contact' className='nav-link'>Contact</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link to='/merch' className='nav-link'>Merch</Link>
-                    </li>
+	const [ open, setOpen ] = useState('false');
+	
+	const handleOpen = () => {
+		setOpen(!open)
+	}
+	const closeMobile = () => {
+		setOpen(false)
+	}
 
-                    
-                </ul>
-            </nav>
-            
-        </div>
-    )
-}
+	return (
+		<div className="navbar">
+			<nav>
+				<div className="nav-list">
+					<ul className={click ? 'nav-options active' : 'nav-options' } >
+						<div className="nav-items">
+							<li onClick={closeMobile}>
+								<Link to="/" className="nav-link">
+									Home
+								</Link>
+							</li>
+							<li onClick={closeMobile}>
+								<Link to="/videos" className="nav-link">
+									Videos
+								</Link>
+							</li>
+							<li onClick={closeMobile}>
+								<Link to="/about" className="nav-link">
+									About
+								</Link>
+							</li>
+							<li onClick={closeMobile}>
+								<Link to="/contact" className="nav-link">
+									Contact
+								</Link>
+							</li>
+							<li onClick={closeMobile}>
+								<Link to="/merch" className="nav-link">
+									Merch
+								</Link>
+							</li>
+						</div>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
